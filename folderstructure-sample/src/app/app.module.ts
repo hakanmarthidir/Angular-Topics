02ba3aec56f7600c6a428e73c1e9276e1c564c15
Hakan Hidir <hakan_hidir@hotmail.com>
2019-05-13 11:22:10 +0200

    github high severity item

--- folderstructure-sample/src/app/app.module.ts
+++ folderstructure-sample/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductlistComponent } from './modules/products/components/productlist/productlist.component';
+
 
 @NgModule({
   declarations: [
