3ba290a7368aaaf19fb99236078cded81097411b
Hakan Hidir <hakan_hidir@hotmail.com>
2019-05-13 11:09:35 +0200

    added sample files into empty folders.

--- folderstructure-sample/src/app/app.module.ts
+++ folderstructure-sample/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
